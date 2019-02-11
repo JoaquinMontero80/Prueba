4c79693c8855fa2504ef1f5cd24b0a30865da87b
Joaquín <descartes.1980@gmail.com>
2019-02-11 10:22:17 +0100

    aaaa

---
+++ 2.17.c
@@ -0,0 +1,17 @@
+// Programación. Ejercicio 2_17_C. Números del 1 al 4. Joaquín Montero.
+
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	    printf ( "1\n" );
+	    printf ( "2\n" );
+	    printf ( "3\n" );
+	    printf ( "4\n" );
+	    
+
+	
+	return 0;
+}
